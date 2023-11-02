86ead994fb03613749c687d307b3134303acc041
ABHISHEK KUMAR <abhishekkumar@ABHISHEKs-MacBook-Air.local>
2023-11-02 23:11:48 +0530

    working on search

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -45,7 +45,7 @@ const Feed = () => {
       <form className="relative w-full flex-center">
         <input 
           type="text"
-          placeholder="Search for a tag or a username"
+          placeholder="Search for a tag or a username ..... ( DEV_ABHI is working on that )"
           value={searchText}
           onChange={handleSearchChange}
           required
